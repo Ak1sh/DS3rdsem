223c2f162066728b29c7230cd2082bdcc5c5b30a
Ak1sh <93367904+Ak1sh@users.noreply.github.com>
2021-12-14 10:46:29 +0530

    Update linked_list_insertion.c

--- linked_list_insertion.c
+++ linked_list_insertion.c
@@ -47,6 +47,37 @@ void atend(int d)
     ptr->next=NULL;
     }}
 }
+
+void insertatloc(int d)  
+    {  
+        struct node *ptr = (struct node *) malloc (sizeof(struct node));  
+        struct node *temp;  
+        int i,loc;  
+        if(ptr == NULL)  
+        {  
+            printf("\nOVERFLOW");  
+        }  
+        else  
+        {  
+              
+            printf("Enter the location");  
+            scanf("%d",&loc);             
+            ptr->data = d;  
+            temp=head;  
+            for(i=0;i<loc;i++)  
+            {  
+                temp = temp->next;  
+                if(temp == NULL)  
+                {  
+                    printf("\ncan't insert\n");  
+                    return;  
+                }
+            }
+              ptr ->next = temp ->next;   
+            temp ->next = ptr;   
+            printf("\nNode inserted"); 
+        }
+    }
 void display()
 {
     struct node *temp=head;
@@ -65,7 +96,8 @@ int main()
     printf("1.from beginning\n");
     printf("2.from end\n");
     printf("3.Display the elements\n");
-    printf("4.Exit\n");
+    printf("4.specific pos\n");
+    printf("5.Exit\n");
     printf("Enter your choice\n");
     scanf("%d",&s);
     switch(s)
@@ -86,8 +118,12 @@ int main()
         case 3:
         display();
         break;
-        
         case 4:
+        printf("Enter the element \n");
+        scanf("%d",&a);
+        insertatloc(a);
+        break;
+        case 5:
         exit(0);
         break;
         
@@ -100,3 +136,6 @@ int main()
     return 0;
     
 }
+
+
+
