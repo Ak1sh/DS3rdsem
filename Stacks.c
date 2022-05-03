7c36e3d0198895a835f35a962ffaa830f47cfb8e
Ak1sh <93367904+Ak1sh@users.noreply.github.com>
2022-05-03 16:35:36 +0530

    Update Stacks.c

--- Stacks.c
+++ Stacks.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 #define SIZE 10
 int arr[SIZE];
 int top = -1;
@@ -27,11 +28,42 @@ int isFull()
 }
 void push(int k)
 {
-    arr[++top] = k;
+    if(isFull())
+    {
+        printf("Stack is full");
+        }
+        else
+        {
+            arr[++top] = k;
+            printf("inserted\n");
+            }
 } 
-int pop()
+void pop()
 {
-    return (arr[top--]);
+    if(isEmpty())
+    {
+        printf("Stack is empty");
+        
+    }
+    else
+    {
+        arr[top--];
+        printf("Deleted");
+}
+}
+void display()
+{
+    if(isEmpty())
+    {
+        printf("Empty stack");
+    }
+    else
+    {
+    for(int i=top;i>=0;i--)
+    {
+        printf("%d",arr[top]);
+    }
+}
 }
 int main()
 {
@@ -51,15 +83,10 @@ int main()
       push(num);
       break;
       case 2:
-      elem = pop();
-      printf("popped number is %d \n",elem);
+      pop();
       break;
       case 3:
-      for(int i=0; i<= top ; i++)
-       {
-          printf(" %d  ",arr[i]);
-        }
-        printf("\n");
+      display();
       break;
       case 4:
       exit(0);
@@ -69,6 +96,5 @@ int main()
     while(entry != 4);
     return 0;
 }
-
 
 
